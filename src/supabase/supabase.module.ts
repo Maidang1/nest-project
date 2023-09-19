46236f534f946408f18fe7419e2346ea046fcf1c
maidang1 <497350746@qq.com>
2023-09-19 23:25:02 +0800

    添加接口

--- src/supabase/supabase.module.ts
+++ src/supabase/supabase.module.ts
@@ -6,7 +6,10 @@ import { createClient } from '@supabase/supabase-js';
 const SupabaseProvider = [
   {
     provide: 'SUPABASE_CLIENT',
-    useValue: createClient('', ''),
+    useValue: createClient(
+      'https://fxsxedmgqjcfdmjxjbwy.supabase.co',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4c3hlZG1ncWpjZmRtanhqYnd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkzNTU2MTAsImV4cCI6MjAwNDkzMTYxMH0.yFnAzA9mH0keAwzEoG0TVXqSy_ujwVsv7jLpxhV_YSg',
+    ),
   },
 ];
 
